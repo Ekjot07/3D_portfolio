6bbe945063d219b81706f786dbf6e69f8dde2c01
Ekjot Panesar <ekjotsingh2002@gmail.com>
2023-11-02 20:17:34 +0530

    fix: computer canvas on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,8 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
+const isMobile = window.innerWidth <= 768;
+
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
@@ -24,7 +26,7 @@ const Hero = () => {
         </div>
       </div>
       
-      <ComputersCanvas />
+      {!isMobile && <ComputersCanvas />}
 
       <div
         className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center"
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,19 +3,8 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { tech } from "../constants";
 
-const isMobile = window.innerWidth <= 768;
-
 const Tech = () => {
-  return isMobile ? (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {tech.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-          <p className="techName">{technology.name}</p>
-        </div>
-      ))}
-    </div>
-  ) : (
+  return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
